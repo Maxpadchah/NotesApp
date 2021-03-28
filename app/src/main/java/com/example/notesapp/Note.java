06aa687f2a39db14ed4ec71ad4b84c1ed189a44b
Maxpadchah <75752983+Maxpadchah@users.noreply.github.com>
2021-03-28 22:26:37 +0300

    The task is completely completed

---
+++ app/src/main/java/com/example/notesapp/Note.java
@@ -0,0 +1,16 @@
+package com.example.notesapp;
+
+public class Note {
+    private String nameNotes;
+    private String textNotes;
+    private String dateNotes;
+
+
+    public Note(String nameNotes, String textNotes, String dateNotes) {
+        this.nameNotes = nameNotes;
+        this.textNotes = textNotes;
+        this.dateNotes = dateNotes;
+    }
+
+
+}
